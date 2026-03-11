7949c36657c83251e271f0664c63edeaa92c1580
wirakan90-ai <wirakan90@gmail.com>
2026-03-11 19:01:22 +0700

    Update certificate.js

--- watboad.org/js/certificate.js
+++ watboad.org/js/certificate.js
@@ -1,1 +1,36 @@
+// ระบบค้นหาใบอนุโมทนาบัตร
 
+function searchCertificate(){
+
+let keyword = document.getElementById("search").value;
+
+let donors = JSON.parse(localStorage.getItem("donors")) || [];
+
+let result = document.getElementById("result");
+
+result.innerHTML = "";
+
+let found = donors.filter(function(d){
+return d.name.includes(keyword);
+});
+
+if(found.length === 0){
+
+result.innerHTML = "ไม่พบข้อมูล";
+
+return;
+
+}
+
+found.forEach(function(d){
+
+result.innerHTML +=
+"<div>" +
+"<b>"+d.name+"</b><br>" +
+"จำนวนเงิน: "+d.amount+" บาท<br>" +
+"วันที่: "+d.date +
+"</div><hr>";
+
+});
+
+}
